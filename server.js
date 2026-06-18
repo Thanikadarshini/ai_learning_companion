d11c31d2f2e92a91f202f6265a4fd7be4b395ef7
Thanikadarshini Iyappan <ithanika30@gmail.com>
2026-06-18 19:53:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const Groq = require("groq-sdk");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const groq = new Groq({
+apiKey: process.env.GROQ_API_KEY
+});
+
+app.post("/chat", async (req, res) => {
+
+try {
+
+const userMessage = req.body.message;
+
+const response =
+await groq.chat.completions.create({
+
+messages: [
+{
+role: "system",
+content:
+"You are an educational AI tutor. Explain clearly with examples."
+},
+{
+role: "user",
+content: userMessage
+}
+],
+
+model: "llama-3.1-8b-instant"
+
+});
+
+res.json({
+reply:
+response.choices[0].message.content
+});
+
+} catch(error){
+
+console.error("FULL ERROR:");
+console.error(error);
+
+res.status(500).json({
+reply: error.message
+});
+
+}
+
+});
+
+app.listen(3000, () => {
+console.log(
+"Server running on http://localhost:3000"
+);
+});
