5257c94cb8251b9e90e03be985b538bddcb3fd19
Thanikadarshini Iyappan <ithanika30@gmail.com>
2026-06-18 20:43:06 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+app.use(express.static(__dirname));
 
 const groq = new Groq({
 apiKey: process.env.GROQ_API_KEY
