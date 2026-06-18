e4c53ce3c29934342cf4fb637618cd706af6552a
Thanikadarshini Iyappan <ithanika30@gmail.com>
2026-06-18 20:04:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 const groq = new Groq({
 apiKey: process.env.GROQ_API_KEY
@@ -57,8 +57,8 @@ reply: error.message
 
 });
 
-app.listen(3000, () => {
-console.log(
-"Server running on http://localhost:3000"
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
